43110b280480706c84ce77b59c7128f68def977e
Berat Karataşoğlu <beratc237@gmail.com>
2025-05-23 17:56:32 +0100

    brute force done

--- Code/algorithms/brute_force.cpp
+++ Code/algorithms/brute_force.cpp
@@ -1,8 +1,35 @@
 #include "brute_force.h"
 
 Solution solveBruteForce(const std::vector<Pallet>& pallets, const Truck& truck) {
+    auto start = std::chrono::high_resolution_clock::now();
     Solution solution;
     solution.algorithmName = "Brute Force";
-    // TODO: Implement brute force algorithm
+    solution.truck = truck;
+    solution.totalProfit = 0;
+
+    int maxWeight = truck.capacity;
+    int n = pallets.size();
+
+    for (int i = 0; i < (1 << n); ++i) {
+        double totalWeight = 0;
+        double totalProfit = 0;
+        std::vector<Pallet> selectedPallets;
+
+        for (int j = 0; j < n; ++j) {
+            if (i & (1 << j)) {
+                totalWeight += pallets[j].weight;
+                totalProfit += pallets[j].profit;
+                selectedPallets.push_back(pallets[j]);
+            }
+        }
+
+        if (totalWeight <= maxWeight && selectedPallets.size() <= truck.maxPallets && totalProfit > solution.totalProfit) {
+            solution.totalProfit = totalProfit;
+            solution.selectedPallets = selectedPallets;
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    solution.executionTime = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     return solution;
-} 
+}
